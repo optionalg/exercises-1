0a38bda8d55b830193898ff4dd055689779e6f87
pychiwo <dr.joint@gmail.com>
2015-04-04 14:32:21 +0200

    Create 4.py

---
+++ 4.py
@@ -0,0 +1,15 @@
+"""
+    Write a function that takes a character (i.e. a string
+    of length 1) and returns True if it is a vowel,
+    False otherwise.
+"""
+
+
+def check_vowel(character):
+    vowels = ("A", "E", "I", "O", "U", "Y")
+    if character in vowels:
+        return True
+    else:
+        return False
+
+print(check_vowel("a".upper()))

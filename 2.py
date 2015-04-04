e725ea833e05b5cbf7b46935204df739e2daada5
pychiwo <dr.joint@gmail.com>
2015-04-04 14:19:37 +0200

    Create 2.py

---
+++ 2.py
@@ -0,0 +1,12 @@
+"""
+    Define a function max_of_three() that takes three
+    numbers as arguments and returns the largest of them.
+"""
+
+
+def max_of_three(num1, num2, num3):
+    numbers = [num1, num2, num3]
+    numbers.sort()
+    return numbers[-1]
+
+print(max_of_three(1, 2, 3))

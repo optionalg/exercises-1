bda3610e672e401fab6014e09e0cbfedfc3395af
pychiwo <dr.joint@gmail.com>
2015-04-04 14:27:07 +0200

    Create 3.py

---
+++ 3.py
@@ -0,0 +1,16 @@
+"""
+    Define a function that computes the length of
+    a given list or string. (It is true that Python
+    has the len() function built in, but writing it
+    yourself is nevertheless a good exercise.)
+"""
+
+
+def length():
+    string = "Python"
+    string_length = 0
+    for i in string:
+        string_length += 1
+    return string_length
+
+print(length())
